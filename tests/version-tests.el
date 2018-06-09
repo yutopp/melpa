61a4dec7cc31ecddd42e2bd9f6d9a1abd771913c
Jonas Bernoulli <jonas@bernoul.li>
2018-06-08 23:26:01 -0500

    [wip]

--- tests/version-tests.el
+++ tests/version-tests.el
@@ -17,29 +17,25 @@
 (package-initialize)
 
 (ert-deftest persistent-versions-stable ()
-  (let ( (package-build-stable t)
-         (package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")))
-         (package-build--recipe-alist (package-build--read-recipes-ignore-errors)))
-
+  (let ((package-build-stable t)
+        (package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/"))))
     (package-refresh-contents)
-
     (cl-loop for recipe in (cdr package-archive-contents)
              do
              (setq recipe (car recipe))
              (should (equal
                       (version-to-list (cadr (package-build-archive recipe)))
-                      (package-desc-version (cadr (assoc recipe package-archive-contents))))))))
+                      (package-desc-version
+		       (cadr (assoc recipe package-archive-contents))))))))
 
 (ert-deftest persistent-versions ()
-  (let ( (package-build-stable nil)
-         (package-archives '(("melpa" . "https://melpa.org/packages/")))
-         (package-build--recipe-alist (package-build--read-recipes-ignore-errors)))
-
+  (let ((package-build-stable nil)
+        (package-archives '(("melpa" . "https://melpa.org/packages/"))))
     (package-refresh-contents)
-
     (cl-loop for recipe in (cdr package-archive-contents)
              do
              (setq recipe (car recipe))
              (should (equal
                       (version-to-list (cadr (package-build-archive recipe)))
-                      (package-desc-version (cadr (assoc recipe package-archive-contents))))))))
+                      (package-desc-version
+		       (cadr (assoc recipe package-archive-contents))))))))
